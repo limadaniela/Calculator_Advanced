b419c6c2fb6e5f024421221b42e9df4b98be070f
limadaniela <62920069+limadaniela@users.noreply.github.com>
2022-12-05 10:21:43 -0600

    Add More Functionality to Calculator

--- Calculator_Advanced/ViewController.swift
+++ Calculator_Advanced/ViewController.swift
@@ -11,20 +11,39 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     
-    var storedValue: String = ""
+    private var storedValue: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         //what should happen when a non-number button is pressed
-        displayLabel.text = ""
-        storedValue = ""
+        storedValue = true
+        
+        //guard is to verify that the conversion from string to double can happen
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double")
+        }
+        //to perform the calculation
+        //as currentTitle is an optional string, use optional binding
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number * 0.01)
+            }
+        }
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         //what should happen when a number is entered into the keypad
         
         if let numValue = sender.currentTitle {
-            displayLabel.text = storedValue + numValue
-            storedValue.append(numValue)
+            if storedValue {
+                displayLabel.text = numValue
+                storedValue = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
         }
     }
 }
